d51f56f17dd4803974954c9734fbd03d16166613
Ryong-E <think8867@gmail.com>
2023-11-11 15:41:28 +0900

    feature: 각 메뉴들을 관리하는 menus 생성
    - 수량과 메뉴에 없는 메뉴에 대해서 에러를 반환
    - 에러메세지 상수화

---
+++ src/models/menu/Menus.js
@@ -0,0 +1,44 @@
+import ValidationError from '../../ValidationError.js';
+import ERROR_MESSAGE from '../../constant.js';
+import AppetizerMenu from './AppetizerMenu.js';
+import DessertMenu from './DessertMenu.js';
+import DrinkMenu from './DrinkMenu.js';
+import MainMenu from './MainMenu.js';
+
+class Menus {
+  #menuCategory;
+
+  constructor() {
+    this.#menuCategory = [new MainMenu(), new AppetizerMenu(), new DessertMenu(), new DrinkMenu()];
+  }
+
+  getOrderState(menuName, quantity) {
+    try {
+      Menus.#quantiryValidate(quantity);
+      const foundMenu = this.#findMenu(menuName, quantity);
+
+      if (foundMenu) return foundMenu;
+
+      throw new ValidationError(ERROR_MESSAGE.not_allow_menu);
+    } catch ({ message }) {
+      throw new ValidationError(message);
+    }
+  }
+
+  #findMenu(menuName, quantity) {
+    let result;
+
+    this.#menuCategory.forEach((category) => {
+      const findMenu = category.getMenuPrice(menuName, quantity);
+      if (findMenu) result = findMenu;
+    });
+
+    return result;
+  }
+
+  static #quantiryValidate(quantity) {
+    if (quantity < 1) throw new ValidationError(ERROR_MESSAGE.not_allow_menu);
+  }
+}
+
+export default Menus;
