a78f17b2ad5453a27c6bdea5fb11fe6f5569c54c
handongryong <think8867@gmail.com>
2023-11-12 18:46:59 +0900

    feature: 할인, 이벤트를 관리하는 Promotion 생성
    할인의 총 결과값에 대한 값 반환하는 기능
    이벤트의 총 결과값에 대한 값 반환하는 기능

---
+++ src/models/promotion/Promotion.js
@@ -0,0 +1,59 @@
+import ChristmasDisCount from './discount/ChristmasDiscount.js';
+import SpecialDiscount from './discount/SpecialDiscount.js';
+import WeekdayDiscount from './discount/WeekdayDiscount.js';
+import WeekendDiscount from './discount/WeekendDiscount.js';
+import PresentEvent from './event/PresentEvent.js';
+
+class Promotion {
+  #discounts;
+
+  #evnets;
+
+  constructor() {
+    this.#discounts = [
+      new ChristmasDisCount(),
+      new WeekdayDiscount(),
+      new WeekendDiscount(),
+      new SpecialDiscount(),
+    ];
+    this.#evnets = [new PresentEvent()];
+  }
+
+  getTotalDiscountResult(reservationDate, order) {
+    const results = this.#getDiscountInfo(reservationDate, order);
+
+    return results;
+  }
+
+  #getDiscountInfo(reservationDate, order) {
+    const results = [];
+
+    this.#discounts.forEach((discount) => {
+      const result = discount.getDiscountResult(reservationDate, order);
+
+      if (result) results.push(result);
+    });
+
+    return results;
+  }
+
+  geTotaltEventResult(order) {
+    const results = this.#getEventResult(order);
+
+    return results;
+  }
+
+  #getEventResult(order) {
+    const results = [];
+
+    this.#evnets.forEach((event) => {
+      const result = event.getPresect(order);
+
+      if (result) results.push(result);
+    });
+
+    return results;
+  }
+}
+
+export default Promotion;
