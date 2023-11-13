7be20e179a319748cf17e23c72ff1cbb21431872
handongryong <think8867@gmail.com>
2023-11-14 00:47:28 +0900

    feature: order controller 생성
    예약날짜,주문메뉴를 받아 저장합니다
    프로모션을 적용합니다
    영수증을 출력합니다

---
+++ src/controllers/OrderController.js
@@ -0,0 +1,57 @@
+import InputView from '../InputView.js';
+import OutputView from '../OutputView.js';
+import { ERROR_MESSAGE } from '../constant.js';
+import Bill from '../models/Bill.js';
+
+class OrderController {
+  #bill;
+
+  async play() {
+    OutputView.printIntro();
+    await this.#askReservationDate();
+    await this.#askOrderMenu();
+    this.#applyPromotion();
+    this.#printBill();
+  }
+
+  async #askReservationDate() {
+    try {
+      const date = await InputView.askReservationDate();
+      this.#bill = new Bill(date);
+    } catch (error) {
+      OutputView.print(ERROR_MESSAGE.not_allow_date);
+      await this.askReservationDate();
+    }
+  }
+
+  async #askOrderMenu() {
+    try {
+      const orders = await InputView.askOrderMenu();
+      this.#bill.setMenu(orders);
+      OutputView.printEventInfo(this.#bill.getReservationDate());
+      OutputView.printMenu(orders);
+    } catch (error) {
+      OutputView.print(ERROR_MESSAGE.not_allow_menu);
+      await this.askOrderMenu();
+    }
+  }
+
+  #applyPromotion() {
+    this.#bill.applyPromotion();
+    this.#bill.checkPresent();
+    this.#bill.issuanceBedge();
+  }
+
+  #printBill() {
+    OutputView.printBeforeDiscountTotalAmount(this.#bill.getTotalOrderAmount());
+    OutputView.printPresent(this.#bill.getPresentList());
+    OutputView.printDiscountDetail(this.#bill.getDiscountList());
+    OutputView.printTotalDiscountAmount(this.#bill.getTotalDiscountAmount());
+    OutputView.printEstimateAmount(
+      this.#bill.getTotalOrderAmount(),
+      this.#bill.getTotalDiscountAmount(),
+    );
+    OutputView.printBedge(this.#bill.getBedge());
+  }
+}
+export default OrderController;
