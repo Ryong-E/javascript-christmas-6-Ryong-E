15c30f544923595dfad7f400b52385456606216a
handongryong <think8867@gmail.com>
2023-11-14 00:59:44 +0900

    fix: 상수값 앞에 공백값으로 인해 테스트코드가 통과하지 않던 부분 수정

--- src/constant.js
+++ src/constant.js
@@ -20,8 +20,8 @@ export const BEDGE_NAME = {
 };
 
 export const ERROR_MESSAGE = {
-  not_allow_date: '유효하지 않은 날짜입니다. 다시 입력해주세요.',
-  not_allow_menu: ' 유효하지 않은 주문입니다. 다시 입력해주세요.',
+  not_allow_date: '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
+  not_allow_menu: '유효하지 않은 주문입니다. 다시 입력해 주세요.',
   not_only_drink: '음료만 주문할 수 없습니다.',
   over_menu_order: '메뉴는 한 번에 최대 20개까지만 주문 가능합니다.',
 };
