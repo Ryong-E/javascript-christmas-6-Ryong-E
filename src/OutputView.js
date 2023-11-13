018893d75d4385645414c6ae30cbfe813b101346
handongryong <think8867@gmail.com>
2023-11-14 00:41:36 +0900

    fix: 빈값일 때 없음이라고 표기해야하는 부분 로직 추가와 각 실행시 하단 공백 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
-import { OUTPUT_MESSAGE, TITLE } from './constant.js';
+import { EMPTY, NOT, OUTPUT_MESSAGE, TITLE } from './constant.js';
 
 const OutputView = {
+  print(message) {
+    Console.print(message);
+  },
+
   printIntro() {
     Console.print(OUTPUT_MESSAGE.intro);
   },
@@ -12,43 +16,58 @@ const OutputView = {
       const { menuName, quantity } = order;
       Console.print(`${menuName} ${quantity}개`);
     });
+    Console.print(EMPTY);
     // ...
   },
   printEventInfo(reservationDate) {
     Console.print(`12월 ${reservationDate}일에 우테코 식당에서 받을 이벤트 혜텍 미리 보기!`);
+    Console.print(EMPTY);
   },
 
   printBeforeDiscountTotalAmount(amount) {
     Console.print(TITLE.before_discount);
-    Console.print(amount);
+    Console.print(`${amount.toLocaleString()}원`);
+    Console.print(EMPTY);
   },
 
   printPresent(present) {
     Console.print(TITLE.present);
-    Console.print(`${present} 1개`);
+    if (present.length === 0) Console.print(NOT);
+    if (present.length > 0) Console.print(`${present.join(',')} ${present.length}개`);
+    Console.print(EMPTY);
   },
 
   printDiscountDetail(discounts) {
-    Console.print(TITLE.before_discount);
-    discounts.forEach((discount) => {
-      const { eventName, discount: discountAmount } = discount;
-      Console.print(`${eventName}: -${discountAmount.toLocaleString()}원`);
-    });
+    Console.print(TITLE.benefit);
+    if (discounts.length === 0) Console.print(NOT);
+    if (discounts.length > 0) {
+      discounts.forEach((discount) => {
+        const { eventName, discount: discountAmount } = discount;
+        Console.print(`${eventName}: -${discountAmount.toLocaleString()}원`);
+      });
+    }
+
+    Console.print(EMPTY);
   },
 
   printTotalDiscountAmount(discountAmount) {
-    Console.print(TITLE.benefit);
-    Console.print(`-${discountAmount.toLocaleString()}원`);
+    Console.print(TITLE.total_benefit);
+    if (discountAmount <= 0) Console.print(NOT);
+    if (discountAmount > 0) Console.print(`-${discountAmount.toLocaleString()}원`);
+    Console.print(EMPTY);
   },
 
   printEstimateAmount(totalOrderAmount, totalDiscountAmount) {
     Console.print(TITLE.after_discount);
-    Console.print(`${(totalOrderAmount - totalDiscountAmount).toLocaleString()}월`);
+    Console.print(`${(totalOrderAmount - totalDiscountAmount).toLocaleString()}원`);
+    Console.print(EMPTY);
   },
 
   printBedge(bedge) {
     Console.print(TITLE.event_bedge);
-    Console.print(bedge);
+    if (bedge === false) Console.print(NOT);
+    if (bedge) Console.print(bedge);
+    Console.print(EMPTY);
   },
 };
 
