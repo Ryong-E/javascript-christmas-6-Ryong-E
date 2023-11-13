33c36c5e1495d51a0b238131b21527c9da36ceb0
handongryong <think8867@gmail.com>
2023-11-13 20:05:42 +0900

    feature: 입력 받은 메뉴를 카테고리별로 저장하는 기능 생성

--- src/models/Bill.js
+++ src/models/Bill.js
@@ -25,5 +25,20 @@ class Bill {
     this.#menu = new Menus();
     this.#promotion = new Promotion();
   }
+
+  setMenu(orders) {
+    orders.forEach((order) => {
+      const { menuName, quantity } = order;
+      const classifiedMenu = this.#menu.getOrderState(menuName, quantity);
+      const { menu, category } = classifiedMenu;
+
+      if (category in this.#orderMenuList) {
+        this.#orderMenuList[category].push(menu);
+        return;
+      }
+
+      this.#orderMenuList[category] = [menu];
+    });
+  }
 }
 export default Bill;
