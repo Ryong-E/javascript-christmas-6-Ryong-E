baed24247c8cdac882ea18fc2fff346df6ef594c
handongryong <think8867@gmail.com>
2023-11-14 22:07:09 +0900

    feature: 주문내역을 관리하는 Order 생성

---
+++ src/models/Order.js
@@ -0,0 +1,94 @@
+import ValidationError from '../ValidationError.js';
+import { ERROR_MESSAGE, ORDER_SETTING } from '../constant.js';
+import Menus from './menu/Menus.js';
+
+class Order {
+  #orderMenuList;
+
+  #menu;
+
+  constructor() {
+    this.#orderMenuList = {};
+    this.#menu = new Menus();
+  }
+
+  setMenu(orders) {
+    Order.#isExceedOrder(orders);
+    this.#allocateMenu(orders);
+    this.#hasOnlyDrink();
+  }
+
+  #allocateMenu(orders) {
+    orders.forEach((order) => {
+      const { menuName, quantity } = order;
+      const classifiedMenu = this.#menu.getOrderState(menuName, quantity);
+      const { menu, category } = classifiedMenu;
+
+      if (category in this.#orderMenuList) {
+        this.#orderMenuList[category].push(menu);
+        return;
+      }
+
+      this.#orderMenuList[category] = [menu];
+    });
+  }
+
+  isLessMininum(callback) {
+    const totalAmount = this.getTotalOrderAmount();
+    if (totalAmount < ORDER_SETTING.min_discount_amount) {
+      callback();
+      return true;
+    }
+
+    return false;
+  }
+
+  #hasOnlyDrink() {
+    const totalKeys = Object.keys(this.#orderMenuList);
+    const hasDrinkKey = totalKeys.includes('drink');
+
+    if (totalKeys.length === 1 && hasDrinkKey) {
+      throw new ValidationError(ERROR_MESSAGE.not_only_drink);
+    }
+
+    return true;
+  }
+
+  static #isExceedOrder(orders) {
+    let orderCount = 0;
+    orders.forEach((order) => {
+      orderCount += Number(order.quantity);
+    });
+
+    if (orderCount > ORDER_SETTING.max_order) {
+      throw new ValidationError(ERROR_MESSAGE.over_menu_order);
+    }
+  }
+
+  getOrderMenuList() {
+    return this.#orderMenuList;
+  }
+
+  getTotalOrderAmount() {
+    let total = 0;
+    const categotys = Object.values(this.#orderMenuList);
+
+    if (categotys.length !== 0) {
+      categotys.forEach((category) => {
+        total += Order.#sumCategoryOrderAmount(category);
+      });
+    }
+
+    return total;
+  }
+
+  static #sumCategoryOrderAmount(category) {
+    let sum = 0;
+    category.forEach((order) => {
+      sum += order.price * order.quantity;
+    });
+
+    return sum;
+  }
+}
+export default Order;
