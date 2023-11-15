934e060e9aa75177db0a9335891a6c1257b9145c
handongryong <think8867@gmail.com>
2023-11-15 21:10:49 +0900

    fix: 상수로 사용되는 값이기에 대문자로 변경하였습니다

--- src/models/promotion/event/PresentEvent.js
+++ src/models/promotion/event/PresentEvent.js
@@ -1,20 +1,20 @@
 class PresentEvent {
-  #eventName;
+  #EVENT_NAME;
 
   #DISCOUNT_BASIS;
 
-  #presentItem;
+  #PRESENT_ITEM;
 
   constructor() {
-    this.#eventName = '증정 이벤트';
-    this.#presentItem = '샴페인';
+    this.#EVENT_NAME = '증정 이벤트';
+    this.#PRESENT_ITEM = '샴페인';
     this.#DISCOUNT_BASIS = 120000;
   }
 
   getPresent(totalDiscount) {
     try {
       this.#isEvent(totalDiscount);
-      return { eventName: this.#eventName, discount: 25000, present: this.#presentItem };
+      return { eventName: this.#EVENT_NAME, discount: 25000, present: this.#PRESENT_ITEM };
     } catch (erorr) {
       return false;
     }
